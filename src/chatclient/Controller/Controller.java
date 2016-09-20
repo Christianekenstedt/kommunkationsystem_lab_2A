f1ab7441978697806f71f174d39a64f27cc28201
christianekenstedt <christian>
2016-09-20 22:06:33 +0200

    Initial commit

---
+++ src/chatclient/Controller/Controller.java
@@ -0,0 +1,25 @@
+package chatclient.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private TextArea outputArea;
+
+    @FXML
+    void sendBtnPressed(ActionEvent event) {
+
+    }
+
+}
---
+++ src/chatclient/Main.java
@@ -0,0 +1,30 @@
+package chatclient;
+
+import chatclient.Controller.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("View/chatclient.fxml")
+        );
+        Parent root = loader.load();
+        primaryStage.setTitle("Chat Client");
+        primaryStage.setScene(new Scene(root, 400, 400));
+
+        Controller controller = loader.getController();
+        //TODO: Ev skicka in controller till någon datamodell och datamodell till controller.
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
